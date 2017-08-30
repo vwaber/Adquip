d2c6982aed45ed07f0a465cc5ee854a24079088c
Vaan W <vwaber@gmail.com>
2017-08-30 10:37:25 -0700

    Created and added new "quips" Java library to project

--- app/src/main/java/com/vwaber/adquip/MainActivity.java
+++ app/src/main/java/com/vwaber/adquip/MainActivity.java
@@ -1,14 +1,12 @@
 package com.vwaber.adquip;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.vwaber.quips.QuipGenerator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        QuipGenerator generator = new QuipGenerator();
+        Toast.makeText(this, generator.getNext(), Toast.LENGTH_SHORT).show();
     }
 
 //    @Override
---
+++ quips/src/main/java/com/vwaber/quips/QuipGenerator.java
@@ -0,0 +1,9 @@
+package com.vwaber.quips;
+
+public class QuipGenerator {
+
+    public String getNext(){
+        return "A stupid default joke or whatever...?";
+    }
+
+}
